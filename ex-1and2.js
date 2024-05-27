261e43cfe2208f13a39a232802cfe48d853b33c9
Emmiiez <emmiiez.m@gmail.com>
2024-05-27 18:05:44 +0700

    Update ex-1and2.js
    Sent Exercise #1+2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let sirName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const foodPrice = 15000;
+const totalRestaurantOpen = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”;
+foodPrice = 5000;
+totalRestaurantOpen = 2500;
